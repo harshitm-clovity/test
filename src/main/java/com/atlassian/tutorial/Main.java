f85629dd78a8975dbcb98cfaf0e8e83dc89e4956
harshitm-clovity <harshitm@clovity.com>
2026-07-22 20:28:12 +0530

    Fix comment formatting in Main.javavvvvvv
    Updated a comment to correct the text and formatting.

--- src/main/java/com/atlassian/tutorial/Main.java
+++ src/main/java/com/atlassian/tutorial/Main.java
@@ -1,6 +1,6 @@
 package com.atlassian.tutorial;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> orghjgjhghhjndjnsmndmsnhjkjhjk
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> orghjgjhghhjndjnsmndmsnhjkjhjkhyjhjhj
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
