4b2ef6642b3f67f8b955c5c03c824588fbfa2b91
harshitm-clovity <harshitm@clovity.com>
2026-07-28 15:42:52 +0530

    Fix comment tip formatting in Main.java
    Corrected a comment tip in the Main.java file.

--- src/main/java/com/atlassian/tutorial/Main.java
+++ src/main/java/com/atlassian/tutorial/Main.java
@@ -1,6 +1,6 @@
 package com.atlassian.tutorial;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> orghjgjhghhjndjnsmndmsnhjkjhjkhyjhjhj
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> orghjgjhghhjndjnsmndmsnhjkjhjkhyjhjhjghgh
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
