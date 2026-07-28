774f8ec5b21956377cbd0aa2646ff1cacb7d9fdc
harshitm-clovity <harshitm@clovity.com>
2026-07-28 17:07:28 +0530

    Clean up comments in Main.java
    Removed unnecessary and unclear comments from Main.java.

--- src/main/java/com/atlassian/tutorial/Main.java
+++ src/main/java/com/atlassian/tutorial/Main.java
@@ -1,7 +1,7 @@
 package com.atlassian.tutorial;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> orghjgjhghhjndjnsmndmsnhjkjhjkhyjhjhjghgh
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.gjhjhj
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
